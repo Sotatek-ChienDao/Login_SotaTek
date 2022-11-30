12b097639bc711c61b41b51f765b771e924404e5
chiendvhust <107022984+chienhust123@users.noreply.github.com>
2022-11-30 21:30:13 +0700

    login facebook

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    facebook_api_key: "718446989219008",
+    facebook_api_secret: "3bd0a5d4e45e6a6f9b643c84747662c2",
+    callback_url: "http://localhost:3000/auth/facebook/callback",
+    use_database: true,
+    host: "localhost",
+    username: "root",
+    password: "",
+    database: "login_passport",
+};
---
+++ routes.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+
+module.exports = (function () {
+    router.get("/", function (req, res) {
+        res.render("index", { user: req.user });
+    });
+
+    router.get("/login", function (req, res) {
+        res.render("loginlocal");
+    });
+
+    router.get("/account", ensureAuthenticated, function (req, res) {
+        res.render("account", { user: req.user });
+    });
+
+    router.get(
+        "/auth/facebook",
+        passport.authenticate("facebook", { scope: "email" })
+    );
+
+    router.get(
+        "/auth/facebook/callback",
+        passport.authenticate("facebook", {
+            successRedirect: "/",
+            failureRedirect: "/login",
+        }),
+        function (req, res) {
+            res.redirect("/");
+        }
+    );
+
+    router.get("/logout", function (req, res) {
+        req.logout(function (err) {
+            if (err) {
+                return next(err);
+            }
+            res.redirect("/");
+        });
+    });
+
+    return router;
+})();
+
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
